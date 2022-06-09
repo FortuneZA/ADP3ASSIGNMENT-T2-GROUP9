b03d0ca76650fa6168cbce1f06e126cc86844e43
Project3Assignment <mfortuin509@gmail.com>
2022-06-09 12:21:44 +0200

    Base code adjustments

--- src/main/java/za/ac/cput/school_management_main.java
+++ src/main/java/za/ac/cput/school_management_main.java
@@ -1,4 +1,12 @@
 package za.ac.cput;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class school_management_main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(school_management_main.class, args);
+    }
 }
