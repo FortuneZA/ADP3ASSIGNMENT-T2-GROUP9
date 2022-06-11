1bf2457e51c09860f127fd735170af330223d46a
Cameron Noemdo <219115443@mycput.ac.za>
2022-06-11 17:26:18 +0200

    Extended JPARepository to student repository interface

--- src/main/java/za/ac/cput/repository/student/IStudentRepository.java
+++ src/main/java/za/ac/cput/repository/student/IStudentRepository.java
@@ -1,4 +1,12 @@
 package za.ac.cput.repository.student;
 
-public interface IStudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.domain.student.Student;
+
+import java.util.List;
+
+public interface IStudentRepository  extends JpaRepository<Student,String> {
+    List<Student> findAll();
+
+    //List<Student> findStudentsByCountryId(String countryId);
 }
