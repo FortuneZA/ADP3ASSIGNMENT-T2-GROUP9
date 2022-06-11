a66f24a0a816034a1418b5fc970f0b9c8ec2bb08
Cameron Noemdo <219115443@mycput.ac.za>
2022-06-11 17:27:19 +0200

    Completed student factory

--- src/main/java/za/ac/cput/factory/student/StudentFactory.java
+++ src/main/java/za/ac/cput/factory/student/StudentFactory.java
@@ -1,4 +1,21 @@
 package za.ac.cput.factory.student;
 
+import org.springframework.util.StringUtils;
+import za.ac.cput.domain.identity.Name;
+import za.ac.cput.domain.student.Student;
+import za.ac.cput.util.GenericHelper;
+
 public class StudentFactory {
+    public static Student newStudent(String studentId, String email, Name name) {
+        if(StringUtils.isEmpty(studentId))
+            throw new IllegalArgumentException("StudentId is not present");
+        if(StringUtils.isEmpty(name))
+            throw new IllegalArgumentException("Name is not present");
+        GenericHelper.emailValidation(email);
+        return new Student.Builder().
+                setStudentId(studentId).
+                setEmail(email).
+                setName(name).
+                build();
+    }
 }
