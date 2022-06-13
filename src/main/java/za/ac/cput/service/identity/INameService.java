0f7855be91e32cfa140c53a10d02c3e8d2eccb9e
Project3Assignment <mfortuin509@gmail.com>
2022-06-13 21:53:10 +0200

    INAMEREPO -findAll() -Excluded findbyLastName() INAMESERVICE -Added method NameService Populated methods

--- src/main/java/za/ac/cput/service/identity/INameService.java
+++ src/main/java/za/ac/cput/service/identity/INameService.java
@@ -3,5 +3,9 @@ package za.ac.cput.service.identity;
 import za.ac.cput.domain.identity.Name;
 import za.ac.cput.service.IService;
 
+import java.util.List;
+
 public interface INameService extends IService<Name,String> {
+
+    List<Name> findAll();
 }
