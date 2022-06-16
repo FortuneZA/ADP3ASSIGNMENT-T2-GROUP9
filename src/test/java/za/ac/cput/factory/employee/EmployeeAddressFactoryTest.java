a692ac7f303d0063d2f09d3d2aaebbcc7fc3d562
Mawande05 <61276643+Mawande05@users.noreply.github.com>
2022-06-16 21:15:18 +0200

    Domain and Factory

--- src/test/java/za/ac/cput/factory/employee/EmployeeAddressFactoryTest.java
+++ src/test/java/za/ac/cput/factory/employee/EmployeeAddressFactoryTest.java
@@ -1,7 +1,52 @@
 package za.ac.cput.factory.employee;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import za.ac.cput.domain.employee.EmployeeAddress;
+import za.ac.cput.domain.location.Address;
+import za.ac.cput.domain.region.City;
+import za.ac.cput.factory.location.AddressFactory;
 
+import static org.junit.jupiter.api.Assertions.*;
+//@TestMethodOrder(MethodOrder.OrderAnnotation.class)
 class EmployeeAddressFactoryTest {
 
+//    private EmployeeAddress employeeAddress;
+//    private City city;
+//    private final Address address = AddressFactory.createAddress("05","Spring view","63","Molokwane st",7800,city);
+//
+//    @Oder(1)
+//    @Test
+//    void buildEmployeeAddressWithSuccess(){
+//        employeeAddress = EmployeeAddressFactory.createEmployeeAddress("219074054",address);
+//        System.out.println(employeeAddress);
+//        assertAll(
+//                ()->assertNotNull(employeeAddress),
+//                ()->assertEquals("219074054",employeeAddress.getAddress())
+//
+//        );
+//    }
+//
+//    @Order(2)
+//    @Test
+//    void buildEmployeeAddressWithFailure(){
+//        Exception exception = assertThrows(IllegalArgumentException.class, ()->
+//                employeeAddress = EmployeeAddressFactory.createEmployeeAddress(null,address));
+//
+//        String exceptionMessage = exception.getMessage();
+//        System.out.println(exceptionMessage);
+//        assertSame("Please provide staff Id", exceptionMessage);
+//    }
+//
+//    @Order(3)
+//    @Test
+//    void buildEmployeeAddressWithFailure(){
+//
+//        Exception exception =assertThrows(IllegalArgumentException.class,()->
+//                employeeAddress = EmployeeAddressFactory.createEmployeeAddress("",address));
+//
+//        String exceptionMessage = exception.getMessage();
+//        System.out.println(exceptionMessage);
+//        assertSame("Please provide staff Id", exceptionMessage);
+//
+//    }
 }
--- src/test/java/za/ac/cput/factory/student/StudentAddressFactoryTest.java
+++ src/test/java/za/ac/cput/factory/student/StudentAddressFactoryTest.java
@@ -1,7 +1,51 @@
 package za.ac.cput.factory.student;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Test;
+import za.ac.cput.domain.location.Address;
+import za.ac.cput.domain.region.City;
+import za.ac.cput.domain.student.Student;
+import za.ac.cput.domain.student.StudentAddress;
+import za.ac.cput.factory.location.AddressFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+//@TestMethodOrder(MethodOrder.OrderAnnotation.class)
 class StudentAddressFactoryTest {
+//    private StudentAddress studentAddress;
+//    private City city;
+//    private final Address address = AddressFactory.createAddress("01","Student Junction","109","Voortrekka road",7800,city);
+//
+//    @Order(1)
+//    @Test
+//    void buildStudentAddressWithSuccess(){
+//        studentAddress = StudentAddressFactory.createStudentAddress("219074052",address);
+//        System.out.println(studentAddress);
+//        assertAll(
+//                ()->assertNotNull(studentAddress),
+//                ()->assertEquals("219074052",studentAddress.getAddress())
+//        );
+//    }
+//
+//    @Oder(2)
+//    @Test
+//    void buildStudentAddressWithFailure(){
+//        Exception exception = assertThrows(IllegalArgumentException.class,()->
+//                studentAddress = StudentAddressFactory.createStudentAddress("",address));
+//        String exceptionMessage = exception.getMessage();
+//        System.out.println(exceptionMessage);
+//        assertSame("Please provide student Id",exceptionMessage);
+//
+//    }
+//
+//    @Oder(3)
+//    @Test
+//    void buildStudentAddressWithFailure(){
+//        Exception exception = assertThrows(IllegalArgumentException.class, ()->
+//                studentAddress = StudentAddressFactory.createStudentAddress(null,address));
+//        String exceptionMessage = exception.getMessage();
+//        System.out.println(exceptionMessage);
+//        assertSame("Please provide student Id", exceptionMessage);
+//    }
+
 
 }
