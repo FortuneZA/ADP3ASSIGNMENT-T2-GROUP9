f0020ef2cbcaf3b8c5d4bbf8bc9cb1921f714b5a
User <217238173@mycput.ac.za>
2022-06-18 22:38:26 +0200

    nineth commit

--- src/test/java/za/ac/cput/service/student/StudentServiceTest.java
+++ src/test/java/za/ac/cput/service/student/StudentServiceTest.java
@@ -1,5 +1,5 @@
 package za.ac.cput.service.student;
-
+/*
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -77,3 +77,5 @@ class StudentServiceTest {
 //        return null;
 //    }
 }
+
+ */
