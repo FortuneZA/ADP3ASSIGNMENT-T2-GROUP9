06434457774ed6a3a5e3153e8d541fc128c8182d
Mawande05 <61276643+Mawande05@users.noreply.github.com>
2022-06-18 15:05:24 +0200

    Completed JPA repo and Service implementation

--- src/main/java/za/ac/cput/service/employee/IEmployeeAddressService.java
+++ src/main/java/za/ac/cput/service/employee/IEmployeeAddressService.java
@@ -1,4 +1,15 @@
+/*
+ * IEmployeeAddressService.java
+ * Service for the Employee address
+ * Author: Mawande Langa (219074054)
+ * */
 package za.ac.cput.service.employee;
 
-public interface IEmployeeAddressService {
+import za.ac.cput.domain.employee.EmployeeAddress;
+import za.ac.cput.service.IService;
+
+import java.util.List;
+
+public interface IEmployeeAddressService extends IService<EmployeeAddress, String> {
+    List<EmployeeAddress> findAll();
 }
--- src/main/java/za/ac/cput/service/student/StudentAddressService.java
+++ src/main/java/za/ac/cput/service/student/StudentAddressService.java
@@ -1,4 +1,43 @@
+/*
+ * StudentAddressService.java
+ * Service implementation for the Student address
+ * Author: Mawande Langa (219074054)
+ * */
 package za.ac.cput.service.student;
 
-public class StudentAddressService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.student.StudentAddress;
+import za.ac.cput.repository.student.IStudentAddressRepository;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class StudentAddressService implements IStudentAddressService{
+    private final IStudentAddressRepository repository;
+
+    @Autowired
+    public StudentAddressService(IStudentAddressRepository repository){
+        this.repository=repository;
+    }
+    @Override
+    public StudentAddress save(StudentAddress studentAddress) {
+        return this.repository.save(studentAddress);
+    }
+
+    @Override
+    public Optional<StudentAddress> read(String studentId) {
+        return this.repository.findById(studentId);
+    }
+
+    @Override
+    public void delete(StudentAddress studentAddress) {
+        this.repository.delete(studentAddress);
+
+    }
+
+    @Override
+    public List<StudentAddress> findAll() {
+        return this.repository.findAll();
+    }
 }
